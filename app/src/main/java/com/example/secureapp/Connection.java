4c8171ab5d944d5b1de2b71c41933d8f463b54f7
Aga Zabaleta <aga.zabaleta.mabolo@gmail.com>
2020-05-25 19:13:03 +0200

    main menu

--- app/src/main/java/com/example/secureapp/Connection.java
+++ app/src/main/java/com/example/secureapp/Connection.java
@@ -2,7 +2,10 @@ package com.example.secureapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Connection extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class Connection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
+
+        Button clickButton = (Button) findViewById(R.id.button_connect);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Intent myIntent = new Intent(Connection.this, ListeLocaux.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                Connection.this.startActivity(myIntent);
+            }
+        });
     }
 }
