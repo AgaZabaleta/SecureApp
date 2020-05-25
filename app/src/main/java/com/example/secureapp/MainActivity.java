8ab2208692fca752a3c05aeecd16c144b4fb9b6d
Aga Zabaleta <aga.zabaleta.mabolo@gmail.com>
2020-05-25 20:19:28 +0200

    basic database

--- app/src/main/java/com/example/secureapp/MainActivity.java
+++ app/src/main/java/com/example/secureapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.secureapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -16,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SQLiteDatabase mydatabase = openOrCreateDatabase("database1",MODE_PRIVATE,null);
+        mydatabase.execSQL("DROP TABLE IF EXISTS Locaux;");
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS Locaux(id INT PRIMARY KEY, name VARCHAR, history VARCHAR, lat FLOAT, lon FLOAT);");
+
+        mydatabase.execSQL("INSERT INTO Locaux VALUES(0,'poitiers', 'action:date;action:date', 0.0, 0.0);");
+        Cursor c = mydatabase.query("Locaux", null, "name = 'poitiers'", null, null, null, null, null);
+        c.moveToFirst();
+        int id = c.getInt(0);
+        double lat = c.getDouble(3);
 
         Button clickButton = (Button) findViewById(R.id.start_button);
         clickButton.setOnClickListener( new View.OnClickListener() {
